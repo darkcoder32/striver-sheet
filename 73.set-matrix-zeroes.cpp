2170ce854fd52c4aafb1e76ab46b9f541769e70d
Sumit Kumar <sumit.kumar@docquity.com>
2023-03-23 02:20:22 +0530

    2

--- 73.set-matrix-zeroes.cpp
+++ 73.set-matrix-zeroes.cpp
@@ -6,19 +6,38 @@
 // @lc code=start
 class Solution {
 public:
+        // O(m+n) space O(mn) time
+        // vector<int>bufferX(matrix.size(), 0);
+        // vector<int>bufferY(matrix[0].size(), 0);
+        // for(int i=0; i<matrix.size(); i++) {
+        //     for (int j=0; j<matrix[i].size(); j++) {
+        //         if (matrix[i][j] == 0) {
+        //             bufferX[i] = 1;
+        //             bufferY[j] = 1;
+        //         }
+        //     }
+        // }
+
+        // for(int i=0; i<matrix.size(); i++) {
+        //     for (int j=0; j<matrix[i].size(); j++) {
+        //         if (bufferX[i] == 1 || bufferY[j] == 1) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
     void setZeroes(vector<vector<int>>& matrix) {
         for(int i=0; i<matrix.size(); i++) {
             for (int j=0; j<matrix[i].size(); j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[0][j] = -1;
-                    matrix[i][0] = -1;
+                    bufferX[i] = 1;
+                    bufferY[j] = 1;
                 }
             }
         }
 
         for(int i=0; i<matrix.size(); i++) {
             for (int j=0; j<matrix[i].size(); j++) {
-                if (matrix[i][0] == -1 || matrix[0][j] == -1) {
+                if (bufferX[i] == 1 || bufferY[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
