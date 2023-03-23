960c83197b267cb2c69ecaa437732881812d6146
Sumit Kumar <sumit.kumar@docquity.com>
2023-03-23 18:20:22 +0530

    2

--- 73.set-matrix-zeroes.cpp
+++ 73.set-matrix-zeroes.cpp
@@ -26,20 +26,49 @@ public:
         //     }
         // }
     void setZeroes(vector<vector<int>>& matrix) {
-        for(int i=0; i<matrix.size(); i++) {
-            for (int j=0; j<matrix[i].size(); j++) {
-                if (matrix[i][j] == 0) {
-                    bufferX[i] = 1;
-                    bufferY[j] = 1;
+// time - O(m*n), space - O(1)
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool flag1 = false;
+        bool flag2 = false;
+
+        for (int j=0; j<n; j++) {
+            if (matrix[0][j] == 0) {
+                flag1 = true;
+            }
+        }
+
+        for (int i=0; i<m; i++) {
+            if (matrix[i][0] == 0) {
+                flag2 = true;
+            }
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[0][j] = matrix[i][0] = 0;
                 }
             }
         }
 
-        for(int i=0; i<matrix.size(); i++) {
-            for (int j=0; j<matrix[i].size(); j++) {
-                if (bufferX[i] == 1 || bufferY[j] == 1) {
+        for (int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
-                }
+                } 
+            }
+        }
+
+        if (flag1) {
+            for(int j=0; j<n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (flag2) {
+            for(int i=0; i<m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
